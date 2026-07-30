4344f612f503f564a7c7aa4da23e7ce0f7a07dd6
Zach Auerbach <auernachzach@gmail.com>
2026-07-30 12:51:05 -0700

    deploying app to home page

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // relative base: the app serves both standalone and as a built copy under
+  // the Aurelius site at /galaxy/ (same pattern as /helix/ and /ikos/)
+  base: './',
   plugins: [react()],
 })
